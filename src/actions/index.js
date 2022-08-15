28d44fbef05fe768bea640511f19b3b341a524ed
Luis Urdaneta <luisurdaneta1983@gmail.com>
2022-08-14 22:16:03 -0400

    Faltan detalles finales

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const LOADING = "LOADING";
-export const SWITCH_LANGUAGE = "SWITCH_LANGUAGE"
-export const DARK_MODE = "DARK_MODE"
+export const SWITCH_LANGUAGE = "SWITCH_LANGUAGE";
+export const DARK_MODE = "DARK_MODE";
 export const GET_COUNTRIES = "GET_COUNTRIES";
 export const GET_COUNTRIES_BY_NAME = "GET_COUNTRIES_BY_NAME";
 export const GET_COUNTRY_BY_ID = "GET_COUNTRY_BY_ID";
@@ -13,6 +13,8 @@ export const FILTER_BY_CONTINENT = "FILTER_BY_CONTINENT";
 export const COUNTRIES_BY_ACTIVITIES = "COUNTRIES_BY_ACTIVITIES";
 export const GET_ACTIVITIES = "GET_ACTIVITIES";
 export const POST_ACTIVITIES = "POST_ACTIVITIES";
+export const DELETE_ACTIVITY = "DELETE_ACTIVITY";
+export const UPDATE_ACTIVITY = "UPDATE_ACTIVITY";
 
 export function removeAccents(str) {
   return str
@@ -23,7 +25,7 @@ export function removeAccents(str) {
 
 export function loading() {
   return {
-    type: LOADING
+    type: LOADING,
   };
 }
 
@@ -31,20 +33,20 @@ export function switchLanguage(payload) {
   return {
     type: SWITCH_LANGUAGE,
     payload,
-  }
+  };
 }
 
 export function darkModeAction() {
   return {
     type: DARK_MODE,
-  }
+  };
 }
 
 export function getCountries() {
   return async function (dispatch) {
     dispatch(loading());
     try {
-      const { data } = await axios.get("https://countries-api-deploy.herokuapp.com/countries");
+      const { data } = await axios.get("http://localhost:3001/countries");
       dispatch({
         type: GET_COUNTRIES,
         payload: data,
@@ -62,7 +64,7 @@ export function getByName(name, language) {
       const nameToSearch = language ? removeAccents(name) : name.toLowerCase();
       const languageString = language.toString();
       const { data } = await axios.get(
-        `https://countries-api-deploy.herokuapp.com/countries?name=${nameToSearch}&language=${languageString}`
+        `http://localhost:3001/countries?name=${nameToSearch}&language=${languageString}`
       );
       dispatch({
         type: GET_COUNTRIES_BY_NAME,
@@ -78,7 +80,7 @@ export function getById(id) {
   return async function (dispatch) {
     dispatch(loading());
     try {
-      const { data } = await axios.get(`https://countries-api-deploy.herokuapp.com/countries/${id}`);
+      const { data } = await axios.get(`http://localhost:3001/countries/${id}`);
       dispatch({
         type: GET_COUNTRY_BY_ID,
         payload: data,
@@ -92,7 +94,7 @@ export function getById(id) {
 export function getCountriesNames() {
   return async function (dispatch) {
     try {
-      const { data } = await axios.get("https://countries-api-deploy.herokuapp.com/countries");
+      const { data } = await axios.get("http://localhost:3001/countries");
       dispatch({
         type: GET_COUNTRIES_NAMES,
         payload: data,
@@ -100,7 +102,7 @@ export function getCountriesNames() {
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 }
 
 export function orderByName(payload) {
@@ -131,12 +133,14 @@ export function countriesByActivities(payload) {
   };
 }
 
-export function postActivities(activities) {
+export function postActivities(activities, language) {
   return async function (dispatch) {
     try {
+      console.log(activities);
+      console.log(language);
       const { data } = await axios.post(
-        "https://countries-api-deploy.herokuapp.com/activities",
-        activities
+        "http://localhost:3001/activities/create",
+        activities, language
       );
       dispatch({
         type: POST_ACTIVITIES,
@@ -151,13 +155,46 @@ export function postActivities(activities) {
 export function getActivities() {
   return async function (dispatch) {
     try {
-      const { data } = await axios.get("https://countries-api-deploy.herokuapp.com/activities");
+      const { data } = await axios.get("http://localhost:3001/activities");
       return dispatch({
         type: GET_ACTIVITIES,
         payload: data,
       });
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
   };
+}
+
+export function deleteActivity(id) {
+  return async function (dispatch) {
+    try {
+      const { data } = await axios.delete(
+        `http://localhost:3001/activities/${id}`,
+      );
+      dispatch({
+        type: DELETE_ACTIVITY,
+        payload: data,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+export function putActivity(input, inputValue, language) {
+  return async function (dispatch) {
+    try {
+      const { data } = await axios.put(
+        `http://localhost:3001/activities/update/${input}`,
+        inputValue, language
+      );
+      dispatch({
+        type: UPDATE_ACTIVITY,
+        payload: data,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
