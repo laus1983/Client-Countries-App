736ce2be67f5d8b670a156f632b0ab25e20243cd
Luis Urdaneta <luisurdaneta1983@gmail.com>
2022-08-16 22:37:30 -0400

    Aplicacion lista

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -7,13 +7,20 @@ import "./LandingPage.css";
 export default function LandingPage() {
   const language = useSelector((state) => state.language);
 
+
+
   return (
     <div className="landing">
       <div className="title-landing">
         {language ? (
-          <h1 className="title-landing-h1">Bienvenido a la App de Países</h1>
+          <h1 className="title-landing-h">Bienvenido a CountryTravel</h1>
         ) : (
-          <h1>Welcome to the Countries App</h1>
+          <h1>Welcome to CountryTravel</h1>
+        )}
+        {language ? (
+          <h3 className="title-landing-h">El mundo necesita más exploradores y menos turistas</h3>
+        ):(
+          <h3>The world needs more explorers and fewer tourists</h3>
         )}
       </div>
       <div className="linkTo">
